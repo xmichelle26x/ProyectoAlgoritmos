6af369895ad80869ac6677f57204454330174a4d
MICHELLE-LENOVO\Michelle <xmichelle26x@gmail.com>
2018-11-26 23:55:27 -0500

    insertion

--- algoritmos.py
+++ algoritmos.py
@@ -29,7 +29,34 @@ def quickSortHelper(alist,first,last):
        quickSortHelper(alist,splitpoint+1,last)
 
 
+def partition(alist,first,last):
+   pivotvalue = alist[first]
 
+   leftmark = first+1
+   rightmark = last
+
+   done = False
+   while not done:
+
+       while leftmark <= rightmark and alist[leftmark] <= pivotvalue:
+           leftmark = leftmark + 1
+
+       while alist[rightmark] >= pivotvalue and rightmark >= leftmark:
+           rightmark = rightmark -1
+
+       if rightmark < leftmark:
+           done = True
+       else:
+           temp = alist[leftmark]
+           alist[leftmark] = alist[rightmark]
+           alist[rightmark] = temp
+
+   temp = alist[first]
+   alist[first] = alist[rightmark]
+   alist[rightmark] = temp
+
+
+   return rightmark
 
 
 tiempo = time.time()
